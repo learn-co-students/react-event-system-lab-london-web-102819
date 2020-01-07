3911395e2cf74d01f81ca0227c12f18a4e9e4b43
Faris Aziz <farisaziz12@gmail.com>
2020-01-07 16:31:05 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { component, Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+    logFoc = () => {
+        console.log('Good!')
+    }
+
+    logBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+  render(){
+      return(
+          <button onFocus={this.logFoc} onBlur={this.logBlur}> </button>
+      )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+
+    log = () => {
+        console.log('Entering password...')
+    }
+
+    render(){
+        return(
+            <div>
+            <input type="password" onKeyUp={this.log}> </input>
+            </div>
+        )
+    }
+
+}
