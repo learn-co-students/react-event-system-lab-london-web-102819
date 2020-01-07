d048dc983ec1880726303144438528fd76ce6730
Anastasia Pankina <siapankina@gmail.com>
2020-01-07 20:36:41 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+  handleBlur = event => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  handleFocus = event => {
+    console.log("Good!");
+  };
+  render() {
+    return (
+      <button onBlur={this.handleBlur} onFocus={this.handleFocus}>
+        Eyes on me!
+      </button>
+    );
+  }
+}
